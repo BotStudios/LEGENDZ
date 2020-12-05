319ed93b3f1290b32f9f69fc877be532c5b438be
Joe Lee Official <70639311+joeleeofficial@users.noreply.github.com>
2020-12-05 17:51:55 +0800

    Create blog.js

---
+++ blog/blog.js
@@ -0,0 +1,49 @@
+var app = new Vue({ 
+    el: '#one',
+    data: {
+       title: 'TL'
+    }
+});
+var app = new Vue({ 
+    el: '#onecontent',
+    data: {
+        content: 'Aaron'
+    }
+});
+var app = new Vue({ 
+    el: '#two',
+    data: {
+        title: 'Joe'
+    }
+});
+var app = new Vue({ 
+    el: '#twocontent',
+    data: {
+        content: 'Lee'
+    }
+});
+var app = new Vue({ 
+    el: '#tree',
+    data: {
+        title: 'Mr'
+    }
+});
+var app = new Vue({ 
+    el: '#treecontent',
+    data: {
+        content: 'Ice'
+    }
+});
+var app = new Vue({ 
+    el: '#four',
+    data: {
+        title: 'Lil'
+    }
+});
+var app = new Vue({ 
+    el: '#fourcontent',
+    data: {
+        content: 'Lily'
+    }
+});
+
